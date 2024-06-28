77d9f4acdef5663a0e543783a94b4039cc416e71
Shaswat Raj <shaswatraj3@gmail.com>
2024-06-28 20:45:45 +0530

    Update navbar.tsx

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -11,7 +11,7 @@ export default function Navbar({ }): any {
     return (
         <nav className="fixed z-50 flex items-center justify-between w-full h-24 px-4 py-10 backdrop-blur-md bg-background bg-opacity-30 md:px-8 lg:px-12 xl:px-16 2xl:px-24">
             <Link href="/">
-                <Image alt="logo" className="w-40 cursor-pointer dark:invert" src="/images/logo.svg" height={100} width={170} />
+                <Image alt="logo" className="w-40 cursor-pointer dark:invert" src="./images/logo.svg" height={100} width={170} />
             </Link>
             <div className="hidden gap-1 md:gap-2 lg:gap-4 md:flex">
                 <Button variant="ghost" className="font-semibold text-md">
@@ -30,7 +30,7 @@ export default function Navbar({ }): any {
             </div>
             <div className="items-center hidden gap-2 md:flex">
                 <ModeToggle />
-                <Link href="https://github.com/benlhachemi/modifio.git">
+                <Link href="https://github.com/SopKit/file-converter/.git">
                     <Button
                         variant="default"
                         className="items-center hidden gap-2 bg-orange-600 rounded-full w-fit md:flex"
